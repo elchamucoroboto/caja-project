bb960d88987ee1cfd32c0f25bd9cc4b7fd402cc0
Your Name <you@example.com>
2020-10-23 15:47:40 -0400

    login listo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import func
 import datetime
@@ -17,9 +17,18 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SECRET KEY'] = 'eleazar'
+app.config['SECRET_KEY'] = 'eleazar'
 db = SQLAlchemy(app)
 
+#LOGIN MANAGER
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 # MODELS
@@ -46,23 +55,75 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String, nullable=False)
-    admin = db.Column(db.Boolean)
+    
 
 
 
-    def __init__(self, username, password):
-        
-        self.username = username
-        self.password = generate_password_hash(password, method='sha256')
-        self.admin = False
 
 
     
 #ROUTES 
 
-@app.route('/')
+@app.route('/signup', methods=['POST', 'GET'])
+@login_required
+def signup():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        #si el usuario existe, lo devuelve a la pagina de registro
+        if user:
+            flash('Usuario ya existe')
+            return redirect(url_for('signup'))
+
+        #crea usuario nuevo
+        new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template('signup.html')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/', methods=['POST','GET'])
+def login():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('USUARIO O CLAVE EQUIVOCADA, INTENTE DE NUEVO')
+            return redirect(url_for('login'))
+
+        
+        login_user(user)
+        return (redirect(url_for('home')))
+
+        
+
+
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/home', methods=['GET', 'POST'])
+@login_required
 def home():
 
+    
     today = datetime.date.today()
     operations = Operacion.query.filter(Operacion.date_created == today).all()
     ops = db.session.query(Operacion).all()
@@ -123,6 +184,7 @@ def home():
     venta_total_dolares = currencyFormat(venta_total_dolares), venta_total_bolivares = currencyFormat(venta_total_bolivares), sumEfectivoD = currencyFormat(sumEfectivoD), fondoCajaD = currencyFormat(fondoCajaD), fondoCajaBs = currencyFormat(fondoCajaBs))
 
 @app.route('/create', methods=['POST'])
+@login_required
 def create():
     amount = request.form['amount']
     method = request.form['method']
@@ -148,6 +210,7 @@ def create():
 
 
 @app.route('/informes', methods=['POST', 'GET'])
+@login_required
 def informes():
 
     if request.method == 'POST':
@@ -166,23 +229,7 @@ def informes():
 
 
 
-'''
 
-@app.route('/delete/<id>')
-def delete(id):
-    task = Task.query.filter_by(id=int(id)).delete()
-    db.session.commit()
-    return redirect(url_for('home'))
-
-
-@app.route('/done/<id>')
-def done(id):
-    task = Task.query.filter_by(id=int(id)).first()
-    task.done = not(task.done)
-    db.session.commit()
-    return redirect(url_for('home'))
-
-'''
 
 #Start App
 
